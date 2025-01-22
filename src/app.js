027ff54259663a689111c0d909cb2082409dc1bd
MalicknND <ndiayemalick062@gmail.com>
2025-01-22 10:57:57 +0100

    initialisation

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
