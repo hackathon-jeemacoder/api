d76b21c20f4ff525602a999ddb1bd9063cb9d2a1
MalicknND <ndiayemalick062@gmail.com>
2025-01-22 14:46:20 +0100

    fix bug husky

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,7 +1,9 @@
+// .husky/install.mjs
+
 // Skip Husky install in production and CI
 if (process.env.APP_ENV === 'production' || process.env.CI === 'true') {
   process.exit(0);
 }
 
-const husky = require('husky');
+import husky from 'husky';
 console.log(husky());
