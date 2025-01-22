1d5c0bcd92541c6e69978bcd8278bf21f6ac100b
MalicknND <ndiayemalick062@gmail.com>
2025-01-22 11:23:14 +0100

    initialisation

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      // Ajoutez vos règles ici
+    },
+    ignores: ['node_modules', 'dist', 'build'],
+  },
+];
