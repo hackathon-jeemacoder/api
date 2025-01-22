fa6839970cc8bfbdd9b9bba144492dace6e52ca6
MalicknND <ndiayemalick062@gmail.com>
2025-01-22 11:09:15 +0100

    initialisation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 const PORT = process.env.PORT;
