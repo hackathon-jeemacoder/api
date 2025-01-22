7322aa35646685177894f98dff2f2e48babe9769
MalicknND <ndiayemalick062@gmail.com>
2025-01-23 00:31:49 +0100

    get user connecté

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  // Récupérer le token directement dans le header
+  const token = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Accès non autorisé. Token manquant ou invalide.' });
+  if (!token) {
+    return res.status(401).json({ message: 'Accès non autorisé. Token manquant.' });
   }
 
-  const token = authHeader.split(' ')[1];
-
   try {
+    // Vérifier et décoder le token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Ajout des infos utilisateur au req
+    req.user = decoded; // Ajouter les informations utilisateur au req
     next();
   } catch (error) {
     console.error('Erreur de vérification du token :', error);
